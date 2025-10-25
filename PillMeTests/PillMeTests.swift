a9f01c430af5f16fd74ecff03fa6f15a6b611bc5
Dimitar Dimitrov <idimitak@gmail.com>
2025-10-25 12:41:10 +0200

    Initial Commit

---
+++ PillMeTests/PillMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PillMeTests.swift
+//  PillMeTests
+//
+//  Created by Dimitar Dimitrov on 25/10/2025.
+//
+
+import Testing
+@testable import PillMe
+
+struct PillMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
